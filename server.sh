8cbf0530b9b488cef9bb4ca13d251c4006b45a15
Juhnowski <juhnowski@gmail.com>
2020-03-13 03:39:47 +0300

    bugfix scripts
    Signed-off-by: Juhnowski <juhnowski@gmail.com>

--- server.sh
+++ server.sh
@@ -1,7 +1,1 @@
-ffmpeg \
-    -re \
-    -i film.mp4 \
-    -an \
-    -c:v copy \
-    -f rtp \
-    "rtp://127.0.0.1:1234"
+ffmpeg -re -i film.mp4 -vcodec libx264 -an -f rtp rtp://127.0.0.1:50041 -vn -acodec aac -f rtp rtp://127.0.0.1:50051
